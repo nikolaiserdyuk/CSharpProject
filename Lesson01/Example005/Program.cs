a1b3040f78038660bf36ac755608489cb8922900
Nikolai Serdyuk <flaer645@gmail.com>
2022-11-15 12:23:02 +0300

    added the first lesson

---
+++ Lesson01/Example005/Program.cs
@@ -0,0 +1,8 @@
+﻿int number1 = new Random().Next(1, 10);     // 1 2 3 4 5 6 7 8 9 
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
+int number3 = new Random().Next(1, 10);
+Console.WriteLine(number3);
+int result = number1 + number2 - number3;
+Console.WriteLine(result);
