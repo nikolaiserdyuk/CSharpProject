b62eba4e21f0c6585709c4b26dcbb4852c33dd81
Nikolai Serdyuk <flaer645@gmail.com>
2022-11-15 12:36:56 +0300

    added the first HomeWork

---
+++ Homework01/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = new Random().Next(-10, 11);
+int b = new Random().Next(-10, 11);
+int max = a;
+int min = b;
+if(a > b) 
+{
+    max = a; min = b;
+    Console.Write("Большее = ");
+    Console.WriteLine(max);
+    Console.Write("Меньшее = ");
+    Console.WriteLine(min);
+}
+else if(b > a) 
+{
+    max = b; min = a;
+    Console.Write("Большее = ");
+    Console.WriteLine(max);
+    Console.Write("Меньшее = ");
+    Console.WriteLine(min);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
---
+++ Homework01/Exercise02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задание 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = new Random().Next(1, 101);
+int b = new Random().Next(1, 101);
+int c = new Random().Next(1, 101);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Максимальное из трех чисел = ");
+Console.WriteLine(max);
