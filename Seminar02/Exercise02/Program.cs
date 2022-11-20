3bc42bb17e7f41ce2e91cc9c1276036e2b8b499d
Nikolai Serdyuk <flaer645@gmail.com>
2022-11-20 20:17:48 +0300

    added the second seminar

---
+++ Seminar02/Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿//Нужно написать программу которая выводит случайное число 
+//из отрезка от 10 до 99 и показывает наибольшую цифру этого числа
+
+int number = new Random().Next(10, 100);
+int a = number / 10;
+int b = number % 10;
+if(a > b)
+{
+	Console.WriteLine($"Наибольшая цифра числа {number} является {a}");
+}
+else if(b > a)
+{
+	Console.WriteLine($"Наибольшая цифра числа {number} является {b}");
+}
+else
+{
+	Console.WriteLine($"Цифры числа {number} равны друг другу");
+} 
