157857dec7e8359e4b973978985c0047db63ee17
Nikolai Serdyuk <flaer645@gmail.com>
2022-11-22 16:58:49 +0300

    added the third exercise

---
+++ Homework03/Exercise02/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+//int[] massive = {3, 6, 8, 2, 1, -7};
+int[] massive = {7, -5, 0, 1, -1, 9};
+double distanceX = (Math.Pow((massive[3] - massive[0]), 2)); 
+double distanceY = (Math.Pow((massive[4] - massive[1]), 2));
+double distanceZ = (Math.Pow((massive[5] - massive[2]), 2));
+double distanceR = Math.Sqrt(distanceX + distanceY + distanceZ);
+Console.WriteLine($"{distanceR:f2}");
